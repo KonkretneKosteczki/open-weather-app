000c12ce67cb73a4d13767913414937f6d58e251
krzys <krzysztof.krzeszewski@tdsoft.pl>
2019-11-30 14:15:59 +0100

    moved port settings to config file

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    appPort: process.env.PORT || 4000,
     databaseURI: process.env.MONGODB_URI || "mongodb://localhost:27017/weather-data",
     weatherCheckingCronPattern: "0 0 */12 * * *", // 0s, 0m, 0/12h
     weatherCitiesToCheck: ["Warszawa", "Szczecin", "Lodz", "Wroclaw", "Gdansk", "Krakow", "Suwalki", "Rzeszow"],
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const activateCronJob = require("./weather-api");
+const config = require("./config");
 
 app.use(express.static(__dirname + "/public"));
 app.use("/api", require("./routers/api"));
 app.get("/*", (req, res) => {
     res.status(404).json({error: "endpoint not found"})
 });
-app.listen(4000, () => {
+
+app.listen(config.appPort, () => {
     activateCronJob();
-    console.log("APP listening on port 4000");
+    console.log(`APP listening on port ${config.appPort}`);
 });
