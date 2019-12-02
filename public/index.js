c19424e97c43f63c1eb8b7af507ce6503d99d324
krzys <krzysztof.krzeszewski@tdsoft.pl>
2019-12-02 12:50:02 +0100

    divided index.html into separate stylesheets scripts and html files

---
+++ public/index.js
@@ -0,0 +1,71 @@
+function checkStatus(res) {
+    if (!res.ok) throw "Error requesting data from api";
+    return res;
+}
+
+const options = {method: "POST"};
+fetch("/api/dbSize", options)
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(({days}) => document.getElementById("days").max = days)
+    .catch(console.error);
+
+function createTableFromJSON(json, divContainer) {
+    // EXTRACT VALUE FOR HTML HEADER.
+    // ('Book ID', 'Book Name', 'Category' and 'Price')
+    const columns = ["properties", ...json.map(({city, country}) => city || country)];
+    const table = document.createElement("table");
+    const tr = table.insertRow(-1);
+
+    columns.forEach(label => {
+        const th = document.createElement("th");
+        th.innerHTML = label;
+        tr.appendChild(th);
+    });
+
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    const properties = Object.keys(json[0]).filter(labels => labels !== "city");
+
+    properties.forEach(key => {
+        const tr = table.insertRow(-1);
+        const tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = key;
+
+        json.map(values => {
+            const tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = (Math.round(values[key] * 100) / 100).toString();
+        })
+    });
+
+    divContainer.appendChild(table);
+}
+
+function createTable() {
+    const divContainer = document.getElementById("showData");
+    divContainer.innerHTML = "";
+    return getData().then(data => createTableFromJSON(data, divContainer)).catch(console.error);
+}
+
+async function getData() {
+    const days = document.getElementById("days").value;
+    const cities = await fetch(`/api/cities?d=${days}`, options)
+        .then(checkStatus)
+        .then(res => res.json());
+
+    const data = [];
+    for (const city of cities) {
+        data.push(await fetch(`/api/average?d=${days}&c=${city}`, options)
+            .then(checkStatus)
+            .then(res => res.json())
+            .then(({average}) => ({...average, city}))
+        );
+    }
+
+    data.push(await fetch(`/api/poland?d=${days}`, options)
+        .then(checkStatus)
+        .then(res => res.json())
+        .then(({average}) => ({...average, country: "Poland"}))
+    );
+
+    return data;
+}
